6d667f770613d4eb8c7cb640fff65776ba79919c
XILE-CAI <c358143455@gmail.com>
2022-11-24 23:32:37 +1030

    add errorhandler middleware and connect to mongoDB

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,18 @@
+//allow not to repeat use try catch
+import asynchandler from "express-async-handler"
 
-export const registerUser = (req,res) => {
+export const registerUser = asynchandler( async (req,res) => {
+    const {name, email, password} = req.body
+
+    //validation
+    if(!name || !email || !password){
+        res.status(400)
+        throw new Error('Please include all fields')
+    }
+
     res.send("Register Route")
-}
+})
 
-export const loginUser= (req,res) => {
+export const loginUser= asynchandler( async (req,res) => {
     res.send("Login Route")
-}
+})
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
 import express from 'express'
+import colors from "colors"
 import dotenv from 'dotenv'
 import userRoutes from "./routes/userRoutes.js"
+import errorHandler from "./middleware/errorMiddleware.js"
+import connectDB from "./db/connect.js"
 
 dotenv.config()
+
+//connect to mongoDB
+connectDB()
 
 const PORT = process.env.PORT || 8000
 
 const app = express()
+
+//allow to send json form 
+app.use(express.json())
 
 app.get('/',(req,res) => {
     res.status(200).json({message: "Welcome to the Support Desk API"})
@@ -14,5 +23,8 @@ app.get('/',(req,res) => {
 
 //middleware
 app.use('/api/users', userRoutes)
+
+//error handler middleware
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server run on Port:${PORT}`))
